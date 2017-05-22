f1d2643510e953e8dd46529bdad3b19dd4b50a89
Robert Puskas <rpuskas0@gmail.com>
2017-05-22 19:18:17 +0200

    Deleted some old ugly expired code and added something fresh and juicy with pointers.

--- RomanNumeral/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "roman.h"
-
-int main() {
-    Roman::Roman_int rom;
-    std::cout << "Please type a roman numeral: ";
-    std::cin >> rom;
-    std::cout << std::endl << "Int value of " << rom << " is: " << rom.int_value() << std::endl;
-}
